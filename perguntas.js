fd4f1ac8c0766cab4d03219ee88f4331de5cf53d
maryalara <144155297+maryalara@users.noreply.github.com>
2024-10-28 10:35:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+'Geografia',
+'Questão 1: Quais são as fontes de energia não renováveis?',
+'Resposta = petróleo, carvão mineral, gás natural e nuclear.')
+        
+criaCartao(
+'Historia',
+'Questão 2: Quais são as características do mercantilismo?',
+'Resposta = Acúmulo de metais preciosos, incentivo à manufatura, intervenção do Estado na economia, balança comercial favorável e protecionismo.')
+        
+criaCartao(
+'Matemática',
+'Questão 3: Qual é o antecessor de x-2?',
+'Resposta = x-1')
+
+criaCartao(
+'Matematica',
+'Questão 4: Qual é a raiz quadrada de 144?',
+'Resposta = 12')
+
+criaCartao(
+'Quimica',
+'Questão 5: Quantos carbonos tem em uma cadeia de Cafeina?',
+'Resposta = 8')
+
+criaCartao(
+'Geografia',
+'Questão 6: Em qual continente fica Madagascar?',
+'Resposta = África')
+
+criaCartao(
+'Historia',
+'Questão 7: Em que ano Adolf Hitler morreu?',
+'Resposta = 1945')
+
+criaCartao(
+'Biologia',
+'Questão 8: O que faz uma organela?',
+'Resposta = Uma organela faz a digestão intracelular')
+
+criaCartao(
+'Português',
+'Questão 9: Quais são as características do realismo?',
+'Resposta = Valorização da objetividade e dos fatos. Impessoalidade, apagamento das ideias do autor.')
+
+criaCartao(
+'Biologia',
+'Questão 10: Qual o nome do orgão que sua principal tarefa é filtrar o sangue?',
+'Resposta = Rim')
+
+criaCartao(
+'Matematica',
+'Questão 11: Qual é o nome da função de primeiro grau?',
+'Resposta = Função Afim')
+    
+criaCartao(
+'Quimica',
+'Questão 12: Porque ocorre uma ligação covalente?',
+'Resposta = Ocorre para que cada átomo atinja sua estabilidade, com presença de oito elétrons em sua camada de valência.')
+    
+criaCartao(
+'Geografia',
+'Questão 13: Qual é a primeira capital do Brasil?',
+'Resposta = Salvador')
+    
+criaCartao(
+'Historia',
+'Questão 14: Em que ano ocorreu a primeira revolução industrial?',
+'Resposta = 1760')
+    
+criaCartao(
+'Matemática',
+'Questão 15: Como é representado uma incógnita na matemática?',
+'Resposta = É representado com "x"')
+    
+criaCartao(
+'Português',
+'Questão 16: O que é a metáfora?',
+'Resposta = É uma figura de linguagem; mais particularmente, uma figura de palavra, pois apresenta uma palavra (ou expressão) com sentido figurado.')
+
+criaCartao(
+'Matematica',
+'Questão 17: Qual o valor de PI?',
+'Resposta = 3,14')
+        
+criaCartao(
+'Quimica',
+'Questão 18: Qual cientista propôs o primeiro modelo atômico moderno que ficou conhecido como “bola de bilhar”?',
+'Resposta = John Dalton')
+        
+criaCartao(
+'Geografia',
+'Questão 19: Como é causado um terremoto?',
+'Resposta = São provocados por movimentos na crosta terrestre, composta por enormes placas de rocha, chamadas de placas tectônicas.')
+        
+criaCartao(
+'Historia',
+'Questão 20: Quanto tempo durou a Era Vargas?',
+'Resposta = 15 Anos')
